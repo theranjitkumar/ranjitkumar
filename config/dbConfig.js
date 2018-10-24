1aeedd6bd56f2fab6597a25a003cc76e9659972e
theranjitkumar <web.ranjitkumar@gmail.com>
2018-10-24 11:01:20 +0530

    db updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   // db: 'mongodb://localhost:27017/ranjit',
-  // db: 'mongodb://localhost/ranjit',
-  db: 'mongodb://maurya:maurya123@ds135852.mlab.com:35852/maurya'
+  db: 'mongodb://localhost/ranjit',
+  // db: 'mongodb://maurya:maurya123@ds135852.mlab.com:35852/maurya'
 };
